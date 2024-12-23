ddc800aa87646bf2cb4c538743f24a541bbf6018
Dibri Nsofor <dibrinsofor@gmail.com>
2024-12-23 23:46:13 +0100

    wrote tests for matching
    Signed-off-by: Dibri Nsofor <dibrinsofor@gmail.com>

--- tests/test_pattern_match.py
+++ tests/test_pattern_match.py
@@ -1,13 +1,26 @@
+from src.parse_patterns import Func
 from tests.utils import (
     assert_match,
     check_file
 )
 
 def test_match_def() -> None:
-    check_file("match_def", "def")
+    assert_match("def", 
+                 '''
+curve_ball_def = 1234
+
+def one() -> int:
+    return 1
+
+def two() -> int:
+    return 2
+
+def three() -> int:
+    return 3''', 
+                 3, Func(None, None, False))
 
 def test_empty() -> None:
     source = expected = ""
     pattern = "def $some"
 
-    assert_match("def $some", source, expected)
+    assert_match("def $some", source, 0)
